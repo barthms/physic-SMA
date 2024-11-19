6dea57044d6a6421731e54a3e16c887b0ec3b607
monalisa <monalisadeaanastasya@gmail.com>
2024-11-19 09:54:28 +0700

    membuat sidebar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Beranda from "./pages/Beranda";
-import 'bootstrap/dist/css/bootstrap.css';
-
+import Sidebar from "./components/Sidebar";
+import Hooke from './pages/Hooke';
+import 'bootstrap/dist/css/bootstrap.css'; 
 
 const App = () => {
     return (
+        <> 
             <Router>
-                <Routes>
-                    <Route path="/" element={<Beranda/>} />
-                    <Route path="beranda" element={<Beranda/>} />
-                </Routes>
+                <div className="d-flex vh-110">
+                    <div className="sidebar bg-light p-3" style={{ minWidth: '250px'}}><Sidebar /></div>
+                    <div className="content flex-grow-1 d-flex flex-column">
+                        <div className="flex-grow-1">
+                        <Routes>
+                            <Route path="/" element={<Beranda/>} />
+                            <Route path="/pages/Hooke" element={<Hooke/>} />
+                        </Routes>
+                        </div>
+                    </div>
+                </div>
             </Router>
-    )
+        </>
+    );
 }
 
 export default App;
