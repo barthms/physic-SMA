4c144bf0948e503dbe275a16c7e2027a32632005
Andika <andikabartolomeus677@gmail.com>
2024-11-19 13:06:40 +0700

    memperbaiki komponen sidebar

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
+// App.js
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import 'bootstrap/dist/css/bootstrap.css';
+
 import Beranda from "./pages/Beranda";
 import Newtonn from "./pages/Newtonn";
 import Newton1 from "./pages/newton1";
 import Sidebar from "./components/Sidebar";
 import Hooke from './pages/Hooke';
-
 import Usaha from "./pages/Usaha";
 import GerakLurus from "./pages/GerakLurus";
 
-
 const App = () => {
-
     return (
-        <>
-            <Router>
-                <div className="d-flex vh-110">
-                    <div className="sidebar bg-light p-3" style={{ minWidth: '250px' }}><Sidebar /></div>
-                    <div className="content flex-grow-1 d-flex flex-column">
-                        <div className="flex-grow-1">
-                            <Routes>
-                                <Route path="/" element={<Beranda />} />
-                                <Route path="/pages/Hooke" element={<Hooke />} />
-                                <Route path="/" element={<Beranda />} />
-                                <Route path="beranda" element={<Beranda />} />
-                                <Route path="newtonn" element={<Newtonn />} />
-                                <Route path="newton1" element={<Newton1 />} />
-                                <Route path="usaha" element={<Usaha />} />
-                            </Routes>
-                        </div>
-                    </div>
+        <Router>
+            <div>
+                <Sidebar />
+
+                <div className="main-content">
+                    <Routes>
+                        <Route path="/" element={<Beranda />} />
+                        <Route path="/beranda" element={<Beranda />} />
+                        <Route path="/pages/Hooke" element={<Hooke />} />
+                        <Route path="/newtonn" element={<Newtonn />} />
+                        <Route path="/newton1" element={<Newton1 />} />
+                        <Route path="/newton2" element={<Newtonn />} />
+                        <Route path="/newton3" element={<Newtonn />} />
+                        <Route path="/usaha" element={<Usaha />} />
+                        <Route path="/geraklurus" element={<GerakLurus />} />
+                        <Route path="/GLB" element={<GerakLurus />} />
+                        <Route path="/GLBB" element={<GerakLurus />} />
+                    </Routes>
                 </div>
-            </Router>
-        </>
-    )
+            </div>
+        </Router>
+    );
 };
+
+export default App;
