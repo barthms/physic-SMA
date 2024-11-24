2afc1793ab3fa9238e68b9f8f68a38475a2589d2
rimamutiaragultom <tiarah2104@gmail.com>
2024-11-24 11:25:11 +0700

    perubahan pada gerak melingkar

--- src/pages/GerakLurus.js
+++ src/pages/GerakLurus.js
@@ -1,91 +1,125 @@
-import React from "react";
-import Perpindahan from '../assets/foto2.jpeg';
-import Jarak from '../assets/contoh.jpg';
-// import "./GerakLurus.css";
+import React from 'react';
+import { Container, Row, Col,Card, } from 'react-bootstrap';
+import gm1 from '../assets/gm1.png';
+import gm2 from '../assets/gm2.png';
+import gm3 from '../assets/gm3.jpg';
+import gm4 from '../assets/gm4.png';
 
 
-const GerakLurus = () => {
-  const data = [
-    {
+const FerrisWheel =() => {
+    return (
+        <div className="ferris-wheel-container">
+            <div className="ferris-wheel-icon" style={{ transform: "rotate(0deg) translate(-50%, -140%"}}>
+            <img src="https:/img.icons8.com/color/48/feris-wheel.png" />
+        </div>
+        <div className="ferris-wheel-icon" style={{ transform: "rotate(90deg) translate(-50%, -140%)"}}>
+            <img src="https:/img.icons8.com/color/48/feris-wheel.png" />
+        </div>
+        <div className="ferris-wheel-icon" style={{ transform: "rotate(180deg) translate(-50%, -140%)"}}>
+            <img src="https://img.icons8.com/color/48/ferris-wheel.png" />
+        </div>
+        <div className="ferris-wheel-icon" style={{ transform: "rotate(270deg) translate(-50%, -140%)"}}>
+            <img src="https://img.icons8.com/color/48/ferris-wheel.png" />
+        </div>
+        </div>
+
+        );
+    };
     
-
-      title:"Pengertian Gerak Lurus",
-      content: (
-      <>
-      gerak suatu benda pada lintasan yang lurus. Dalam gerak lurus terdapat lima besaran penting yaitu jarak, perpindahan, kelajuan, kecepatan dan percepatan. Sementara itu, pada gerak lurus berdasarkan karakteristik kecepatan gerak benda, gerak lurus dibedakan menjadi dua, yaitu gerak lurus beraturan (GLB) dan gerak lurus berubah beraturan (GLBB). Dan gerak lurus berubah beraturan dalam penerapannya dibedakan menjadi tiga, yaitu gerak jatuh bebas (GJB), gerak vertikal ke bawah (GVB) dan gerak vertikal ke atas (GVA).
-      <h3> Penerapan Gerak lurus dalam kehidupan sehari hari:</h3>
-    <ul>
-      <li>
-        <strong>Transportasi</strong>Konsep gerak lurus digunakan dalam perancangan dan analisis pergerakan kendaraan seperti mobil, kereta api, dan pesawat terbang. Dengan memahami kecepatan, percepatan, dan jarak tempuh, insinyur dapat merancang sistem transportasi yang efisien dan aman.
-      </li>
-      <li>
-        <strong> Olahraga</strong>Dalam olahraga seperti lari atau balap sepeda, atlet dan pelatih menggunakan prinsip gerak lurus untuk mengukur dan meningkatkan kinerja. Kecepatan dan waktu tempuh dihitung untuk menentukan strategi terbaik dalam kompetisi.
-      </li>
-      <li>
-        <strong>Astronomi</strong>Gerak lurus juga diterapkan dalam astronomi untuk mempelajari pergerakan benda langit. Misalnya, pergerakan planet mengelilingi matahari dapat dianalisis menggunakan hukum gerak Newton dan hukum gravitasi
-      </li>
-      <li>
-        <strong>Konstruksi</strong>Dalam bidang konstruksi, insinyur menggunakan konsep gerak lurus untuk memastikan bahwa struktur bangunan dapat menahan beban dan tekanan tanpa mengalami deformasi yang berbahaya.
-      </li>
-    </ul>
+const cardData =[
+    {
+        id:1,
+        title:"Pengertian Gerak melingkar",
+        content:"gerak suatu benda yang lintasannya berupa lingkaran yang mengelilingi suatu titik tetap. Contohnya adalah gerakan Bulan mengelilingi Bumi.",
+    },
+    {
+        id: 2,
+        title:"Frekuensi dan Periode",
+        content:(
+            <>
+            <p><b>Frekuensi (f):</b>Banyaknya putaran yang dilakukan dalam satu detik.</p>
+            <p><b>Periode (T):waktu yang dibutuhkan objek untuk menyelesaikan satu putaran penuh</b> </p>
+            <img src = {gm1} alt="konsep gm1" className="img-fluid rounded"/>
+            <ul>
+                <li><b>n =</b>banyak putaran</li>
+                <li><b>t =</b>waktu (s)</li>
+                <li><b>1 putaran =</b>2 \pi rad (radian)</li>
+                <li><b>1 rpm (rotasi per menit) =</b>\pi / 15.</li>
+            </ul>
+            <p>eriode dan frekuensi dihubungkan dengan persamaan:</p>
+            <img src = {gm2} alt="konsep gm2" className="img-fluid rounded"/>
+            <p>Dimana:
+            <ul>
+            <li><b>T =</b> periode (s)</li>
+            <li><b>f = </b> frekuensi (Hz)</li>
+            </ul>
+        </p>
     </>
     ),
-    },
-    {
-      title:"Jarak",
-      content: (
+  },
+
+   {
+    id: 3,
+    title:"Kecepatan dan Percepatan Gerak Melingkar",
+    content:(
         <>
-      panjang lintasan sesungguhnya yang ditempuh oleh suatu benda dalam waktu tertentu mulai dari posisi awal dan selesai pada posisi akhir. Jarak merupakan besaran skalar karena tidak bergantung pada arah. Oleh karena itu, jarak selalu bernilai positif. Secara matematis, jarak dirumuskan sebagai berikut:
-      <strong>formula: "s = s1 + s2 + s3 + s4 +…+sn",</strong>
-      <p> Untuk memahami konsep jarak, perhatikan bagan berikut ini</p>
-      <img src = {Jarak}/>
-      </>
-      ),
-    },
-    {
-      title: "Perpindahan",
-      content: (
-        <>
-      "perubahan posisi atau kedudukan suatu benda dari keadaan awal ke keadaan akhirnya. Perpindahan merupakan besaran vektor. Perpindahan hanya mempersoalkan jarak antar kedudukan awal dan akhir suatu objek. Besar perpindahan dapat dihitung dengan menggunakan aturan berikut ini."
-      <p>
-      <strong>formula:</strong> "∆s=Jarak terdekat dari posisi awal ke posisi akhir",
-      </p>
-      <p>Untuk lebih memahami konsep perpindahan, perhatikan gambar berikut ini.</p>
-      <img src = {Perpindahan}/>
-      </>
-      ),
-    },
-    {
-      title:"Kelajuan",
-      content: (
-        <>
-      "jarak yang ditempuh tiap satu satuan waktu. Kelajuan merupakan besarnya kecepatan suatu objek. Kelajuan tidak memiliki arah sehingga termasuk besaran skalar. Secara matematis, persamaan kelajuan dituliskan sebagai berikut."
-      <p>
-      <strong>formula:</strong>"v=s/t"
-      </p>
-      <p> 
-      <strong>Keterangan:</strong>
-      <ul>
-      <li>v=Kelajuan rata-rata (m/s)</li>
-      <li>s=Jarak total yang ditempuh (m)</li>
-      <li>t=Waktu tempuh yang diperlukan (s)</li>
-      </ul>
-      </p>
-      </>
-      ),
-    },
-      
-  ];
-  
-  return (
-    <div className="container">
-      {data.map((item, index) => (
-        <div key={index} className="card">
-          <h2>{item.title}</h2>
-          <div>{item.content}</div>
-        </div>
-      ))}
-    </div>
-  );
+        <p>
+        "Pada gerak melingkar terdapat hal penting yang harus kamu perhatikan, yaitu semua persamaan kecepatan dan percepatan selalu menggunakan persamaan kecepatan sudut dan percepatan sudut. Perhatikan gambar lintasan di bawah ini.",
+        </p>
+    <img src = {gm3} alt="konsep gm3" className="img-fluid rounded"/>
+    <p>Kecepatan (<b>(v)</b>) merupakan kecepatan linier atau kecepatan yang biasa kamu jumpai dalam gerak lurus. Kecepatan sudut atau disebut omega (\omega) dan kecepatan linear (v) dihubungkan dengan persamaan:</p>
+    <img src = {gm4} alt="konsep gm4" className="img-fluid rounded" />
+    <p> Dimana: </p>
+    <ul>
+    <li><b>v =</b>  kecepatan linear (m/s)</li>
+    <li><b>r = </b> jari-jari lintasan (m)</li>
+    </ul>
+    <p>Nilai kecepatan sudut dapat dicari jika diketahui frekuensi ataupun periodenya. Untuk mencari nilai kecepatan sudut (\omega) dipakai rumus:</p>
+    <MathJax.Provider>
+        <p>
+            <MathJax.Node  inline formula= {"\\omega =2\\pi f"} />
+        </p>
+        <p>
+            atau
+        </p>
+        <p>
+        <MathJax.Node  inline formula= {"\\omega =frac{2\\pi}{T} "} />
+        </p>
+        <p> Dimana</p>
+        <ul>
+        <li><b>ω</b> = Kecepatan sudut (rad/s)</li>
+        <li><b>π</b> = 22/7 atau 3,14</li>
+        <li><b>f</b> = Frekuensi (Hz)</li>
+        <li><b>T</b> = Periode (detik)</li>
+        </ul>
+    </MathJax.Provider>
+   </>
+    ),
+   },
+];
+
+const GerakMelingkar = () => {
+    return (
+        <Container className= "my-5">
+            <Row>
+                <Col>
+                <h1 className= " text-center mb-4">GerakMelingkar</h1>
+                </Col>
+            </Row>
+            <Row>
+                {cardData.map((card) => (
+                    <col md={4} className="mb-4" key={card.id}>
+                        <Card>
+                            <Card.Header className="bg-primary text-white">
+                        {card.title}
+                        </Card.Header>
+                        <Card.Body>{card.content}</Card.Body>
+                        </Card>
+                    </col>
+                ))}
+            </Row>
+        </Container>
+    );
 };
-export default GerakLurus;
+
+export default GerakMelingkar;
